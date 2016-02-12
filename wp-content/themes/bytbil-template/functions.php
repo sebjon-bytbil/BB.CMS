38ca2a384012b8063f216be3ce999eb88401aabe
Eric Ejneroos <eric.ejneroos@chas.se>
2016-02-12 16:25:21 +0100

    Single offer template

--- wp-content/themes/bytbil-template/functions.php
+++ wp-content/themes/bytbil-template/functions.php
@@ -55,3 +55,42 @@ function bbtemplate_register_menus()
     register_nav_menu('header-menu', __('Main menu'));
 }
 add_action('init', 'bbtemplate_register_menus');
+
+// Hex to RGBA
+function theme_hex2rgba($color, $opacity = false)
+{
+    $default = 'rgb(0,0,0)';
+
+    // Return default if no color provided
+    if (empty($color))
+        return $default;
+
+    // Sanitize $color if "#" is provided
+    if ($color[0] == '#')
+        $color = substr($color, 1);
+
+    // Check if color has 6 or 3 characters and get values
+    if (strlen($color) == 6)
+        $hex = array($color[0] . $color[1], $color[2] . $color[3], $color[4] . $color[5]);
+    elseif (strlen($color) == 3)
+        $hex = array($color[0] . $color[0], $color[1] . $color[1], $color[2] . $color[2]);
+    else
+        return $default;
+
+    // Convert hexadecimal to rgb
+    $rgb = array_map('hexdec', $hex);
+
+    // Check if opacity is set (rgba or rgb)
+    if ($opacity) {
+        if (abs($opacity) > 1)
+            $opacity = 1.0;
+        $output = 'rgba(' . implode(',', $rgb) . ',' . $opacity . ')';
+    } elseif ($opacity == '0') {
+        $output = 'rgba(' . implode(',', $rgb) . ',' . $opacity . ')';
+    } else {
+        $output = 'rgb(' . implode(',', $rgb) . ')';
+    }
+
+    // Return rgb(a) color string
+    return $output;
+}
---
+++ wp-content/themes/bytbil-template/single-offer.php
@@ -0,0 +1,83 @@
+<?php
+get_header();
+?>
+
+<main>
+<?php while (have_posts()) : the_post();
+
+    $skip_header = false;
+    $image = get_field('offer-image');
+    if (!$image)
+        $skip_header = true;
+    else
+        $image = $image['url'];
+
+    // Caption
+    $caption_content = get_field('offer-caption-content');
+
+    if ($caption_content) {
+        $caption_background_color = 'background: transparent;';
+        $caption_background = get_field('offer-caption-color');
+        $caption_opacity = get_field('offer-caption-opacity');
+        $caption_animation = get_field('offer-caption-animation');
+        $caption_border = get_field('offer-caption-border');
+        $caption_position = get_field('offer-caption-position');
+
+        // Caption color
+        if ($caption_opacity !== 100) {
+            $opacity = $caption_opacity * 0.01;
+            $caption_background_color = 'background: ' . theme_hex2rgba($caption_background, $opacity) . ';';
+        } else {
+            $caption_background_color = $caption_background;
+        }
+
+        if ($caption_background === '')
+            $caption_background_color = 'background: transparent;';
+
+        // Caption border
+        if ($caption_border === 'true')
+            $caption_border_color = 'border: 10px solid ' . theme_hex2rgba($caption_background, 0.75) . ';';
+        else
+            $caption_border_color = 'none';
+
+        // Caption style
+        if ($caption_background_color !== '' || $caption_border_color !== '')
+            $caption_style = $caption_background_color . $caption_border_color;
+    }
+    ?>
+
+    <?php if ($image) : ?>
+    <div class="flexslider">
+        <ul class="slides">
+            <li style="display: block;">
+                <img src="<?php echo $image; ?>">
+                <?php if ($caption_content) : ?>
+                <div class="caption-wrapper">
+                    <div class="caption">
+                        <div class="vertical-align-wrapper" style="<?php echo $overlay_background_color; ?>">
+                            <div class="vertical-align <?php echo $caption_position; ?>">
+                                <div class="horizontal-align">
+                                    <div class="caption-contents" style="<?php echo $caption_style; ?>">
+                                    <?php echo $caption_content; ?>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <?php endif; ?>
+            </li>
+        </ul>
+    </div>
+    <?php endif ?>
+
+    <section>
+        <div class="container-fluid wrapper align-center">
+        <?php the_content(); ?>
+        </div>
+    </section>
+
+<?php endwhile; ?>
+</main>
+
+<?php get_footer(); ?>
