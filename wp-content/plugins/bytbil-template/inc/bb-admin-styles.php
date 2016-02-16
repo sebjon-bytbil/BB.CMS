eff208db025b1ee647504b60785af53891fb3b98
Eric Ejneroos <eric.ejneroos@chas.se>
2016-02-16 14:03:32 +0100

    Some admin style changes

--- wp-content/plugins/bytbil-template/inc/bb-admin-styles.php
+++ wp-content/plugins/bytbil-template/inc/bb-admin-styles.php
@@ -2,6 +2,7 @@
 // Load Admin Styles and Scripts
 function bytbil_admin_styles()
 {
+    wp_enqueue_style('ionicons', plugins_url('inc/visualcomposer/assets/css/ionicons.min.css', __FILE__));
     wp_enqueue_style('bytbil_admin', plugins_url('css/bb-hemsida-admin.css', __FILE__));
     wp_enqueue_script('bytbil_admin', plugins_url('js/bb-hemsida-admin.js', __FILE__), array('jquery'));
 }
