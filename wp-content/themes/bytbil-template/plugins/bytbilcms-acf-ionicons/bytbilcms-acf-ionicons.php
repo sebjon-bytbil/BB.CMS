8d3b936ea96d98a1279ce24df5e67c3cf385d115
Eric Ejneroos <eric.ejneroos@chas.se>
2016-03-02 15:04:23 +0100

    Removed unused stuff

--- wp-content/themes/bytbil-template/plugins/bytbilcms-acf-ionicons/bytbilcms-acf-ionicons.php
+++ wp-content/themes/bytbil-template/plugins/bytbilcms-acf-ionicons/bytbilcms-acf-ionicons.php
@@ -6,19 +6,11 @@ class bytbil_acf_ionicons
     {
         // Version 4
         add_action('acf/register_fields', array($this, 'register_fields'));
-
-        // Version 5
-        add_action('acf/include_field_types', array($this, 'include_field_types'));
     }
 
     function register_fields()
     {
         include_once('includes/bytbilcms-acf-ionicons-v4.php');
-    }
-
-    function include_field_types()
-    {
-        include_once('includes/bytbilcms-acf-ionicons-v5.php');
     }
 }
 
--- wp-content/themes/bytbil-template/plugins/bytbilcms-acf-ionicons/includes/bytbilcms-acf-ionicons-v4.php
+++ wp-content/themes/bytbil-template/plugins/bytbilcms-acf-ionicons/includes/bytbilcms-acf-ionicons-v4.php
@@ -32,28 +32,6 @@ class acf_field_ionicons extends acf_field
         ?>
         <tr class="field_option field_option_<?php echo $this->name; ?>">
             <td class="label">
-                <label><?php _e('Default Icon', 'acf'); ?></label>
-            </td>
-            <td>
-                <div class="ion-field-wrapper">
-                    <div class="ion-live-preview"></div>
-                    <?php
-
-                    //do_action('acf/create_field', array(
-                        //'type' => 'select',
-                        //'name' => 'fields[' . $key . '][default_value]',
-                        //'value' => $field['default_value'],
-                        //'class' => 'ionicons',
-                        //'choices' => array_merge(array('null' => __('Select', 'acf')), $field['choices'])
-                    //));
-
-                    ?>
-                </div>
-            </td>
-        </tr>
-
-        <tr class="field_option field_option_<?php echo $this->name; ?>">
-            <td class="label">
                 <label><?php _e('Allow null?', 'acf'); ?></label>
             </td>
             <td>
@@ -107,15 +85,6 @@ class acf_field_ionicons extends acf_field
         wp_enqueue_style('acf-input-ionicons-input', $this->settings['dir'] . '../assets/css/input.css', array(), $this->settings['version']);
         wp_enqueue_style('acf-input-ionicons', $this->settings['dir'] . '../assets/css/ionicons.css', array(), $this->settings['version']);
         wp_enqueue_style('acf-input-select2-css', $this->settings['dir'] . '../assets/css/select2.css', array(), $this->settings['version']);
-    }
-
-    function field_group_admin_enqueue_scripts()
-    {
-        wp_enqueue_script('ionicons-select2', $this->settings['dir'] . '../assets/js/select2/select2.min.js', array(), $this->settings['version']);
-        wp_enqueue_script('ionicons-create-input', $this->settings['dir'] . '../assets/js/create_input.js', array(), $this->settings['version']);
-        wp_enqueue_style('acf-input-ionicons-input', $this->settings['dir'] . '../assets/css/input.css', array(), $this->settings['version']);
-        wp_enqueue_style('acf-input-ionicons', $this->settings['dir'] . '../assets/css/ionicons.css', array(), $this->settings['version']);
-        wp_enqueue_style('acf-input-ionicons-select2-css', $this->settings['dir'] . '../assets/css/select2.css', array(), $this->settings['version']);
     }
 }
 
